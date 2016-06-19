8debe4cfd96bae51ff0ca923493f3fa36ea461f2
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2016-06-19 11:26:54 -0400

    moved functions out of bash setup

---
+++ functions/prj-cd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+function prj-cd {
+  . prj-export-variables
+
+  if [ ! -d "$WORKING_DIR" ]; then
+    echo "working directory not set"
+    exit 1
+  fi
+
+  if [ ! -d "$ARCHIVE_DIR" ]; then
+    echo "archive directory not set"
+    exit 1
+  fi
+
+  if [ -d "$WORKING_DIR$1" ]; then
+    cd $WORKING_DIR$1
+  elif [ -d "$ARCHIVE_DIR$1" ]; then
+    cd $ARCHIVE_DIR$1
+  else
+    echo "project doesn't exists"
+    exit 1
+  fi
+}
---
+++ functions/prj-start.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+function prj-start {
+  . prj-export-variables
+
+  prj-cd $1
+  if [ $? != 0 ]; then
+    exit 1
+  fi
+
+  if [ $EDITOR == 'sublime' ]; then
+    if hash sublime 2>/dev/null; then
+      sublime "$SUBLIME_DIR$1.sublime-project" &
+    elif hash subl 2>/dev/null; then
+      subl "$SUBLIME_DIR$1.sublime-project" &
+    else
+      echo "could not find sublime command"
+    fi
+  elif [ $EDITOR == 'atom' ]; then
+    if hash atom 2>/dev/null; then
+      atom . &
+    else
+      echo "no atom command"
+    fi
+  fi
+
+  if [ $GIT_CLIENT == 'source tree' ]; then
+    if hash stree 2>/dev/null; then
+      stree "$git_folder" &
+    else
+      echo "no git client found"
+    fi
+  fi
+
+  while read -r script; do
+    if hash osascript 2>/dev/null; then
+      osascript -e 'activate application \"iTerm\"'
+      osascript -e 'tell application \"System Events\" to keystroke \"t\" using command down'
+      osascript -e 'tell application \"iTerm\" to tell session -1 of current terminal to write text \"$script\"'
+    else
+      $script
+    fi
+  done < ".config/new-tab-commands"
+
+  ./start.sh
+}
